5662f96a95b15bfdec7116a901035a2d66f33960
wahyuhadi <hrahmatwahyu@gmail.com>
2020-11-18 10:56:04 +0700

    add link predik

--- lewatmana.py
+++ lewatmana.py
@@ -1,7 +1,7 @@
 import datetime
 import requests
 
-
+from time import sleep
 vidioId = 0
 
 
@@ -17,30 +17,63 @@ def parsingLink(link):
     dateenum = times[1]
 
 
-def predict(date):
+def enums(date):
 
     date = datetime.datetime.strptime(date, '%H%M%S') - datetime.timedelta(hours=0, minutes=5, seconds=0)
     return date.strftime("%H%M%S")
         # print (date.strftime("%H%M%S"))
 
+def predict(date):
 
-def chekUrl(url) :
-    res = requests.get(url)
-    if res.status_code == 200 or len(res.text) > 381:
-        print (url, "Link actived")
+    date = datetime.datetime.strptime(date, '%H%M%S') + datetime.timedelta(hours=0, minutes=5, seconds=0)
+    return date.strftime("%H%M%S")
+
+def chekUrl(type, url) :
+    if type == "enum":
+        res = requests.get(url)
+        if res.status_code == 200 or len(res.text) > 381:
+            print ("ENUM : ", url, "Link actived")
+    else :
+        loop = True
+        while loop is True:
+            sleep(3)
+            res = requests.get(url)
+            if res.status_code == 200 or len(res.text) > 381:
+                print ("Predict : ", url, "Link actived")
+                loop = False
 
 
 def main():
-    link = input("Latest link vidio :")
+    link = input("Latest link vidio (link) : ")
     parsingLink(link)
     global dateenum
     global url
     global hours
-    for i in range(40):
-        dateenum = predict(dateenum)
-        urls = url+"video"+hours+"_"+dateenum+".384.mp4"
-        chekUrl(urls)
-    
+    numlink = int(input("Total link enum (number) : "))
+
+
+
+    print ("[+] Enumeration running !")
+    dateenums = dateenum
+    for i in range(numlink):
+
+        dateenums = enums(dateenums)
+        urls = url+"video"+hours+"_"+dateenums+".384.mp4"
+        chekUrl("enum", urls)
+
+
+    print ("\n[+] Predict link running  ........... ctrl+c to close")
+    try:
+        datepredics = dateenum
+
+        while True:
+            datepredics = predict(datepredics)
+            urls = url+"video"+hours+"_"+datepredics+".384.mp4"
+            chekUrl("predict", urls)
+            
+    except KeyboardInterrupt:
+        print("Press Ctrl-C to terminate while statement")
+        pass
 
 
 if  __name__ == "__main__":
